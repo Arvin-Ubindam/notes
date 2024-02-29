5eed8b3b28afb98061df235e0817d9697a36717c
Arvin- Ubindam <ubindamarvin365@gmail.com>
2024-02-29 10:01:01 +0000

    I have difficulties

--- script2.js
+++ script2.js
@@ -1,14 +1,22 @@
 let includeIt = document.querySelector("#hot");
 let textArea = document.querySelector("#add");
-let paragraph = document.createElement("p");
 let newArray = [];
+
+let container = document.querySelector(".container");
 
 includeIt.addEventListener("click", (e) => {
   e.preventDefault();
-  newArray.push(textArea.value);
+
+  let div = document.createElement("div");
+  let p = document.createElement("p")
+  p.textContent = textArea.value;
+  div.append(p)
+
+  newArray.push(div);
 
   console.log(newArray);
+
+  newArray.map((elem) => {
+    container.append(elem);
+  });
 });
-
-paragraph = newArray;
-
